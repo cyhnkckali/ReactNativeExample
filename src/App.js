fd0589e8cec4faa7d874ce1977c43e3fea2f680c
cyhnkckali <ceyhun.kucukali@fornaxsoftware.com>
2023-02-08 11:50:52 +0300

    Set App File

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StatusBar, Platform, SafeAreaView, StyleSheet } from 'react-native'
+import React, { useEffect } from 'react'
 
 const App = () => {
+
+
+    useEffect(() => {
+        StatusBar.setBarStyle("dark-content");
+        if (Platform.OS === 'android') StatusBar.setBackgroundColor("#fff");
+    }, []);
+
     return (
-        <View>
+        <SafeAreaView style={styles.container}>
             <Text>App</Text>
-        </View>
+        </SafeAreaView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    }
+})
 
 export default App
